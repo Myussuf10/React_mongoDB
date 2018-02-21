44b35652601709054d3ac4857e39fd91fc222740
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-21 18:00:28 +0000

    added: react-router-dom config added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const Landing = () => <div>Landing!!</div>;
+const Dashboard = () => <div>Dashboard !!!!</div>;
+const SurveyNew = () => <div>SurveyNew !!!</div>;
+const Header = () => <div>Header !</div>;
 
 const App = () => (
+		<div>
+		<BrowserRouter>
+		<div>
+		<Header /> 
+		<Route exact path="/" component={Landing}/>
+		<Route exact path="/surveys" component={Dashboard}/>
+		<Route path="/surveys/new" component={SurveyNew}/>
+		</div>
+		</BrowserRouter>
+		</div>
+)
 
-	<div>Inside APP!!</div>
 
-);
 
 export default App;
