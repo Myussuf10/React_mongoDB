ab5d2ef561de788b3a85ba3cde11893df9e29f5c
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-21 15:21:32 +0000

    added: redux structure added to the exisiting app

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+import { createStore , applyMiddleware} from 'redux';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import reducers from './reducers';
+
+// Firstly reducers , Initial State and then Middleware
+const store = createStore(reducers, {} ,  applyMiddleware());
+
+ReactDOM.render(
+	<Provider store={store}>
+	<App />
+	</Provider>, 
+	document.getElementById("root")
+	);
