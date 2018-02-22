d194c7ec6aafbb91f42f16dd2d7c932cdbfddd1e
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-22 11:21:43 +0000

    updated: Header component moved to a seperate file

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+
+
+
+class Header extends Component {
+
+
+	render() {
+
+		return (<div>Class Header</div>);
+	}
+}
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,14 @@ import { Provider } from 'react-redux';
 
 import { createStore , applyMiddleware} from 'redux';
 
+import reduxThunk from 'redux-thunk';
+
 import App from './components/App';
 
 import reducers from './reducers';
 
 // Firstly reducers , Initial State and then Middleware
-const store = createStore(reducers, {} ,  applyMiddleware());
+const store = createStore(reducers, {} ,  applyMiddleware(reduxThunk));
 
 ReactDOM.render(
 	<Provider store={store}>
