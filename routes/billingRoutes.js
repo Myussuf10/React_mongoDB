0929d35169e1481fe71d8340c5ca268271077acb
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-23 14:23:57 +0000

    added: credits added to the model along with Stripe charge object

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,12 +3,21 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 
-moudle.exports = (app) => {
+module.exports = (app) => {
 
-app.post('/api/stripe', (req, res) => {
+app.post("/api/stripe", async (req, res) => {
 
+const charge = await stripe.charges.create({
+  amount: 500,
+  currency: "usd",
+  source: req.body.id, 
+  description: "Charge for sending surveys"
+});
 
+req.user.credits += 5 ;
+const user = await req.user.save();
 
+res.send(user);
 });
 
 };
