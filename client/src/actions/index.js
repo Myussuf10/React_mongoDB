a87f5abe3552401b77fb9fc7d5ed42e0a947ac26
aaqibkhaan <aaqibrashid@hotmail.com>
2018-02-23 12:23:08 +0000

    added: handleToken action created added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,6 +7,15 @@ export const fetchUser = () => async dispatch => {
 
 	const res = await axios.get('/api/user')
 
-	await dispatch({type : FETCH_USER , payload : res.data});
+	dispatch({type : FETCH_USER , payload : res.data});
 	
 	}
+
+
+export const handleToken = (token) => async dispatch => {
+	
+	const res = await axios.get('/api/stripe', token);
+
+	  dispatch({type : FETCH_USER , payload : res.data});
+
+}
