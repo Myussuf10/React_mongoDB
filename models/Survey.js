dbbd523e2cf97fe5ff85310c6300cfc60bd17ac7
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-10 14:54:47 +0000

    added: sub-documented Schema added for reciepts to store response from particular user

--- models/Survey.js
+++ models/Survey.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+const RecipientSchema = require("./Recipient");
+
 const surveychema = new Schema({
 
 	title: String,
 	body: String, 
 	subject: String,
-	recipients : [String],
+	recipients : [RecipientSchema],
 	yes : { type: Number , default: 0 },
 	no: { type: Number , default: 0}
 
