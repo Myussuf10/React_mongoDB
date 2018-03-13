55b479ebe3e81e3a7dc632c513344a0fe878a31c
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-13 22:46:44 +0000

    added: SurveyForm component added

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,15 @@
+// SurveyForm for a user to add input
+
+import React , { Component } from 'react';
+
+
+class SurveyForm extends Component {
+
+	render(){
+		return (<div>
+			SurveyForm!! </div>);
+	}
+}
+
+
+export default SurveyForm;
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,15 @@
+// SurveyNew Shows SurveyForm and SurveyFormReview
+
 import React , { Component } from 'react';
+
+import SurveyForm from './SurveyForm'
 
 
 class SurveyNew extends Component {
 
 	render(){
 		return (<div>
-			SurveyNew!! </div>);
+			< SurveyForm/></div>);
 	}
 }
 
