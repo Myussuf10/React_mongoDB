bd1c669d4aef943063f9e14e3d3b08e317c7a58b
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-13 23:55:17 +0000

    added: SurveyField component added

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,16 @@
+// SurveyField renders single input
+
+import React , { Component } from 'react';
+
+
+class SurveyField extends Component {
+
+	render(){
+		return (<div>
+			<input />
+			</div>);
+	}
+}
+
+
+export default SurveyField;
