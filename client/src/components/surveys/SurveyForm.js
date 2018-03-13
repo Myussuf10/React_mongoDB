7c95cf208c6e29727baabcac9b89f1440e5224a1
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-13 23:34:46 +0000

    added: redux form functionality added

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,14 +2,31 @@
 
 import React , { Component } from 'react';
 
+import { reduxForm , Field } from 'redux-form';
+
 
 class SurveyForm extends Component {
 
 	render(){
 		return (<div>
-			SurveyForm!! </div>);
+			<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+			<Field
+
+			type="text" 
+			name="surveyTitle"
+			component = "input"
+
+			/> 
+			<button type="submit">Submit</button>
+			</form>
+
+			</div>);
 	}
 }
 
 
-export default SurveyForm;
+export default reduxForm({
+	form: 'surveyForm'
+
+
+})(SurveyForm);
